c1582be2a4053e0a6f170b17bddde834e3755d85
KozlovAndreyVladimirovich <87354368+KozlovAndreyVladimirovich@users.noreply.github.com>
2021-08-08 08:39:45 +0500

    Update MAIN.py

--- MAIN.py
+++ MAIN.py
@@ -12,21 +12,27 @@ if not os.path.isdir(out):
     os.makedirs(out)
 mas = os.listdir(path)
 for ii,i in enumerate(mas):
-    print('{}/{}'.format(ii+1,len(mas)))
+    print('{}/{}'.format(ii+1,len(mas))) #показывает, сколько папок уже обработано (необязательная строка)
     name = os.path.splitext(i)
     if name[1] == '':
         m = os.listdir(path+i)
         for jj,j in enumerate( m):
             name = os.path.splitext(j)
             if name[1]=='.pdf':
-                data = title.extract(path+i+'\\'+j)
-                text = converter.convert(path+i+'\\'+j)
-                literature = lit.count(text)
+                data = title.extract(path+i+'\\'+j) #считывание ФИО и темы
+                text = converter.convert(path+i+'\\'+j) #перевод в txt
+                literature = lit.count(text) #подсчёт количества источников
 
-                output = out+name[0]+'.txt'
+                output = out+name[0]+'.txt' #запись в txt
                 file = open(output,'w',encoding='utf-8',errors='ignore')
                 file.write(text)
                 file.close()
+                
+                '''
+                data представляет собой массив, содержащий массив с ФИО и тему, т.е. 2 элемента
+                массив с ФИО имеет n элементов с ФИО и пустой n+1 элемент
+                ещё пишу...
+                '''
                 
                 if data[0] is None:
                     pg.add([j,data[0],data[1],data[2],literature])
